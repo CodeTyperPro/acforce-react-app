595dfcbb784f0fd938bef01a5ef2dffe57273fe6
Alfredo Martins <alfredomartins624@gmail.com>
2023-08-18 21:18:14 +0200

    Update index, icon changed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Preferences from "./pages/Preferences/Preferences";
-import Notifications from "./pages/Notifications/Notifications";
-import NoPage from "./pages/NoPage";
 import SideBar from "./components/SideBar/SideBar";
 import Container from "./components/Container/Container";
-import { AudioPlayerContext } from "./components/Audio/AudioPlayerContext";
-import { useEffect, useState } from "react";
 
 function App() {
   return (
